540072a28f7eede7c089ed04ff4b06eccd0b9854
Keyon Ashby <ashbykey000@warren.k12.in.us>
2024-08-13 10:11:35 -0400

    initial commit

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ num_apples = 20
 num_oranges = 15
 print("Number of apples: ", num_apples)
 print("Number of oranges: ", num_oranges)
-num_apples = 20
 num_oranges = 0
 print("Number of apples: ", num_apples)
 print("Number of oranges: ", num_oranges)
